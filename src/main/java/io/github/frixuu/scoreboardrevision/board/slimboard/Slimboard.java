de33b67554652fad632ec5ae8741df1e468f8e2d
Frixuu <kontakt@lukasz.xyz>
2020-08-29 15:17:05 +0200

    Make Slimboard more readable

--- src/main/java/io/github/frixuu/scoreboardrevision/board/slimboard/Slimboard.java
+++ src/main/java/io/github/frixuu/scoreboardrevision/board/slimboard/Slimboard.java
@@ -1,5 +1,6 @@
 package io.github.frixuu.scoreboardrevision.board.slimboard;
 
+import lombok.Getter;
 import lombok.var;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.ChatColor;
@@ -9,113 +10,95 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 import static org.bukkit.ChatColor.getLastColors;
 
 /**
- * Created by Rien on 23-10-2018.
+ * A wrapper for raw Minecraft scoreboard for easier row updates and placeholder support.
+ * @author Rien
  */
 public class Slimboard {
 
     private final Player player;
-    private final FileConfiguration config;
     private final Plugin plugin;
     private final Objective objective;
 
-    private final int linecount;
+    @Getter private final int rowCount;
     private final boolean lineCanBeLonger;
-    private final HashMap<Integer, String> cache = new HashMap<>();
+    private final Map<Integer, String> rowCache = new HashMap<>();
     public Scoreboard board;
 
-    /**
-     * Construct the board
-     *
-     * @param plugin
-     * @param player
-     * @param linecount
-     */
-    public Slimboard(Plugin plugin, FileConfiguration config, Player player, int linecount) {
+    public Slimboard(Plugin plugin, FileConfiguration config, Player player, final int size) {
         this.player = player;
-        this.config = config;
         this.plugin = plugin;
-        this.linecount = linecount;
+        this.rowCount = size;
 
-        this.lineCanBeLonger = config.getBoolean("settings.longline");
+        lineCanBeLonger = config.getBoolean("settings.longline");
 
-        this.board = this.plugin.getServer().getScoreboardManager().getNewScoreboard();
-        this.objective = this.board.registerNewObjective("sb1", "sb2");
-        this.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-        this.objective.setDisplayName("...");
+        var scoreboardManager = Objects.requireNonNull(plugin.getServer().getScoreboardManager());
+        board = scoreboardManager.getNewScoreboard();
 
-        int score = linecount;
-        for (int i = 0; i < linecount; i++) // Loop through the lines
-        {
-            Team t = this.board.registerNewTeam(i + ""); // Create the first team
-            t.addEntry(ChatColor.values()[i] + ""); // Assign the team with a color
+        objective = board.registerNewObjective("sb1", "sb2", "...");
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-            this.objective.getScore(ChatColor.values()[i] + "").setScore(score); // Set the socre number
+        IntStream.range(0, size).forEachOrdered(i -> {
+            var color = ChatColor.values()[i].toString();
+            var team = this.board.registerNewTeam(String.valueOf(i));
+            team.addEntry(color);
+            // In Minecraft, scoreboard elements are shown in the descending order
+            objective.getScore(color).setScore(size - i);
+        });
 
-            score--; // Lower the score number for the next line
-        }
-
-        this.player.setScoreboard(this.board); // Set the board to the player
+        player.setScoreboard(board); // Set the board to the player
     }
 
-    /**
-     * Set the board title
-     *
-     * @param string
-     */
-    public void setTitle(String string) {
+    public void setTitle(String title) {
 
-        if (string == null) {
-            string = "";
+        if (title == null) {
+            title = "";
         }
         // Check if the PAPI plugin is enabled and the string has a placeholder
         if (plugin.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI") &&
-            PlaceholderAPI.containsPlaceholders(string)) {
-            string = PlaceholderAPI.setPlaceholders(player, string); // Run placeholders!
+            PlaceholderAPI.containsPlaceholders(title)) {
+            title = PlaceholderAPI.setPlaceholders(player, title); // Run placeholders!
         }
 
-        if (!cache.containsKey(-1) || !cache.get(-1).equals(string)) {
-            cache.remove(-1); // Remove it from cache, it is different!
-            cache.put(-1, string); // Put this in the cache!
-            objective.setDisplayName(string); // And set the title
+        if (!rowCache.containsKey(-1) || !rowCache.get(-1).equals(title)) {
+            rowCache.remove(-1); // Remove it from cache, it is different!
+            rowCache.put(-1, title); // Put this in the cache!
+            objective.setDisplayName(title); // And set the title
         }
     }
 
-    /**
-     * Set a specific line
-     *
-     * @param line
-     * @param string
-     */
-    public void setLine(int line, String string) {
+    public void setLine(int rowNum, String value) {
 
-        if (string == null) {
-            string = "";
+        if (value == null) {
+            value = "";
         }
 
         // The line hasn't updated?
-        if (cache.containsKey(line) && cache.get(line) == string) {
+        if (rowCache.containsKey(rowNum) && rowCache.get(rowNum).equals(value)) {
             return;
         }
 
         // Line has updated, refresh the cache!
-        cache.remove(line);
-        cache.put(line, string);
+        rowCache.remove(rowNum);
+        rowCache.put(rowNum, value);
 
         // Prepare the string to preserve colors
-        string = lineCanBeLonger ? prep(string) : prepForShortline(string);
+        value = lineCanBeLonger ? prep(value) : prepForShortline(value);
         var allowedLineLength = lineCanBeLonger ? 64 : 16;
-        var parts = convertIntoPieces(string, allowedLineLength);
+        var parts = convertIntoPieces(value, allowedLineLength);
 
-        Team team = board.getTeam((line) + "");
+        var team = Objects.requireNonNull(board.getTeam(String.valueOf(rowNum)));
+
         if (lineCanBeLonger) {
             team.setPrefix(parts.get(0));
             team.setSuffix(parts.get(1));
@@ -136,7 +119,7 @@ public class Slimboard {
         return parts.get(0) + "§f" + getLastColors(parts.get(0)) + parts.get(1);
     }
 
-    private String prepForShortline(String color) {
+    private static String prepForShortline(String color) {
         if (color.length() <= 16) {
             return color;
         }
@@ -144,7 +127,7 @@ public class Slimboard {
         return pieces.get(0) + "§f" + getLastColors(pieces.get(0)) + pieces.get(1);
     }
 
-    private List<String> convertIntoPieces(String input, int allowedLineLength) {
+    private static List<String> convertIntoPieces(String input, int allowedLineLength) {
         if (ChatColor.stripColor(input).length() <= allowedLineLength) {
             return Arrays.asList(input, "");
         }
